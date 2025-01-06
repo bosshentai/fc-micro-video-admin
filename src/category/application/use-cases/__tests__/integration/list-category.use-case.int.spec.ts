b4f4bb9b8751309c8fc9b4a92c5a604a0567ce9c
Bosshentai <baptistamhernani@gmail.com>
2025-01-06 11:50:55 +0000

    Finish the category challenge

---
+++ src/category/application/use-cases/__tests__/integration/list-category.use-case.int.spec.ts
@@ -0,0 +1,36 @@
+import { setupSequelize } from "../../../../../shared/infra/testing/helpers";
+import { Category } from "../../../../domain/category.entity";
+import { CategorySequelizeRepository } from "../../../../infra/db/sequelize/category-sequelize.repository";
+import { CategoryModel } from "../../../../infra/db/sequelize/category.model";
+import { CategoryOutputMapper } from "../../common/category-output";
+import { ListCategoriesUseCase } from "../../list-category.use-case";
+
+describe("ListCategories Integration Tests", () => {
+  let useCase: ListCategoriesUseCase;
+  let repository: CategorySequelizeRepository;
+
+  setupSequelize({ models: [CategoryModel] });
+
+  beforeEach(() => {
+    repository = new CategorySequelizeRepository(CategoryModel);
+    useCase = new ListCategoriesUseCase(repository);
+  });
+
+  it("should return out sorted bt created_at when input param is empty", async () => {
+    const categories = Category.fake()
+      .theCategories(2)
+      .withCreatedAt((i) => new Date(new Date().getTime() + 1000 + i))
+      .build();
+
+    await repository.bulkInsert(categories);
+
+    const output = await useCase.execute({});
+    expect(output).toEqual({
+      items: [...categories].reverse().map(CategoryOutputMapper.toOutput),
+      total: 2,
+      current_page: 1,
+      per_page: 15,
+      last_page: 1,
+    });
+  });
+});
