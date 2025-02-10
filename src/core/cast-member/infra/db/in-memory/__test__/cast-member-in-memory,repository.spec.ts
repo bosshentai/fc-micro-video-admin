d08e0005b090ef25f3183d7acd7d19957232af6e
Bosshentai <baptistamhernani@gmail.com>
2025-02-10 20:58:33 +0000

    add more test for the cast-member-in-memory.repository.spec.ts

--- src/core/cast-member/infra/db/in-memory/__test__/cast-member-in-memory,repository.spec.ts
+++ src/core/cast-member/infra/db/in-memory/__test__/cast-member-in-memory,repository.spec.ts
@@ -37,17 +37,63 @@ describe('CastMemberIdInMemoryRepository Unit Tests', () => {
     expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
   });
 
-  // it('should filter by type', async () => {
-  //   const faker = CastMember.fake();
+  it('should filter by type', async () => {
+    const faker = CastMember.fake();
 
-  //   const items = [faker.aDirector().build(), faker.anActor().build()];
+    const items = [faker.aDirector().build(), faker.anActor().build()];
 
-  //   const filterSpy = jest.spyOn(items, 'filter' as any);
-  //   const itemsFiltered = await repository['applyFilter'](items, {
-  //     type: CastMemberType.createDirector(),
-  //   });
+    const filterSpy = jest.spyOn(items, 'filter' as any);
+    let itemsFiltered = await repository['applyFilter'](items, {
+      type: CastMemberType.createDirector(),
+    });
 
-  //   expect(filterSpy).toHaveBeenCalledTimes(1);
-  //   expect(itemsFiltered).toStrictEqual([items[0]]);
-  // });
+    expect(filterSpy).toHaveBeenCalledTimes(1);
+    expect(itemsFiltered).toStrictEqual([items[0]]);
+
+    itemsFiltered = await repository['applyFilter'](items, {
+      type: CastMemberType.createActor(),
+    });
+
+    expect(filterSpy).toHaveBeenCalledTimes(2);
+    expect(itemsFiltered).toStrictEqual([items[1]]);
+  });
+
+  it('should filter items by name and type', async () => {
+    const items = [
+      CastMember.fake().anActor().withName('test').build(),
+      CastMember.fake().anActor().withName('fake').build(),
+      CastMember.fake().aDirector().build(),
+      CastMember.fake().aDirector().withName('test fake').build(),
+    ];
+
+    const itemsFiltered = await repository['applyFilter'](items, {
+      name: 'test',
+      type: CastMemberType.createActor(),
+    });
+
+    expect(itemsFiltered).toStrictEqual([items[0]]);
+  });
+
+  it('should sort by created_at when sort para is null', async () => {
+    const items = [
+      CastMember.fake()
+        .anActor()
+        .withName('test')
+        .withCreatedAt(new Date())
+        .build(),
+      CastMember.fake()
+        .anActor()
+        .withName('TEST')
+        .withCreatedAt(new Date(new Date().getTime() + 100))
+        .build(),
+      CastMember.fake()
+        .anActor()
+        .withName('fake')
+        .withCreatedAt(new Date(new Date().getTime() + 200))
+        .build(),
+    ];
+
+    const itemsSorted = await repository['applySort'](items, null, null);
+    expect(itemsSorted).toStrictEqual([items[2], items[1], items[0]]);
+  });
 });
