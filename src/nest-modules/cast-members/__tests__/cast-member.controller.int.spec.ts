de08449278f0bb5e5c78ecc82be0838c010d005d
Bosshentai <baptistamhernani@gmail.com>
2025-02-20 17:54:17 +0000

    finish create all the test for cast-member-controller

--- src/nest-modules/cast-members/__tests__/cast-member.controller.int.spec.ts
+++ src/nest-modules/cast-members/__tests__/cast-member.controller.int.spec.ts
@@ -12,11 +12,15 @@ import { GetCastMemberUseCase } from '@core/cast-member/application/use-cases/ge
 import { DeleteCastMemberUseCase } from '@core/cast-member/application/use-cases/delete-cast-member/delete-cast-member.use-case';
 import {
   CreateCastMemberFixture,
+  ListCastMembersFixture,
   updateCastMemberFixture,
 } from '../testing/cast-member-fixture';
 import { Uuid } from '@core/shared/domain/value-objects/uuid.vo';
 import { CastMemberOutputMapper } from '@core/cast-member/application/use-cases/common/cast-member-output';
-import { CastMemberPresenter } from '../cast-members.presenter';
+import {
+  CastMemberCollectionPresenter,
+  CastMemberPresenter,
+} from '../cast-members.presenter';
 import { CastMember } from '@core/cast-member/domain/cast-member.entity';
 
 describe('CastMembers Integration Tests', () => {
@@ -91,5 +95,76 @@ describe('CastMembers Integration Tests', () => {
         expect(presenter).toEqual(new CastMemberPresenter(output));
       },
     );
+  });
+
+  it('should delete a cast member', async () => {
+    const castMember = CastMember.fake().anActor().build();
+    await repository.insert(castMember);
+
+    const response = await controller.remove(castMember.cast_member_id.id);
+    expect(response).not.toBeDefined();
+    await expect(
+      repository.findById(castMember.cast_member_id),
+    ).resolves.toBeNull();
+  });
+
+  it('should get a cast member', async () => {
+    const castMember = CastMember.fake().anActor().build();
+    await repository.insert(castMember);
+
+    const presenter = await controller.findOne(castMember.cast_member_id.id);
+    expect(presenter.id).toBe(castMember.cast_member_id.id);
+    expect(presenter.name).toBe(castMember.name);
+    expect(presenter.type).toBe(castMember.member_type.type);
+    expect(presenter.created_at).toStrictEqual(castMember.created_at);
+  });
+
+  describe('search method', () => {
+    describe('should sorted cast member by created_at', () => {
+      const { entitiesMap, arrange } =
+        ListCastMembersFixture.arrangeIncrementWithCreatedAt();
+
+      beforeEach(async () => {
+        await repository.bulkInsert(Object.values(entitiesMap));
+      });
+
+      test.each(arrange)(
+        'when send_data is $send_data',
+        async ({ send_data, expected }) => {
+          const presenter = await controller.search(send_data);
+
+          const { entities, ...paginationProps } = expected;
+
+          expect(presenter).toEqual(
+            new CastMemberCollectionPresenter({
+              items: entities.map(CastMemberOutputMapper.toOutput),
+              ...paginationProps.meta,
+            }),
+          );
+        },
+      );
+    });
+
+    describe('should return cast members using pagination, sort and filter', () => {
+      const { entitiesMap, arrange } = ListCastMembersFixture.arrangeUnsorted();
+
+      beforeEach(async () => {
+        await repository.bulkInsert(Object.values(entitiesMap));
+      });
+
+      test.each(arrange)(
+        `when send_data is $send_data`,
+        async ({ send_data, expected }) => {
+          const presenter = await controller.search(send_data);
+          const { entities, ...paginationProps } = expected;
+          expect(presenter).toEqual(
+            new CastMemberCollectionPresenter({
+              items: entities.map(CastMemberOutputMapper.toOutput),
+              ...paginationProps.meta,
+            }),
+          );
+        },
+      );
+    });
   });
 });
--- src/nest-modules/cast-members/testing/cast-member-fixture.ts
+++ src/nest-modules/cast-members/testing/cast-member-fixture.ts
@@ -74,3 +74,117 @@ export class updateCastMemberFixture {
     ];
   }
 }
+
+export class ListCastMembersFixture {
+  static arrangeIncrementWithCreatedAt() {
+    const _entities = CastMember.fake()
+      .theActors(4)
+      .withName((i) => i + '')
+      .withCreatedAt((i) => new Date(new Date().getTime() + 2000 * i))
+      .build();
+
+    const entitiesMap = {
+      first: _entities[0],
+      second: _entities[1],
+      third: _entities[2],
+      fourth: _entities[3],
+    };
+
+    const arrange = [
+      {
+        send_data: {},
+        expected: {
+          entities: [
+            entitiesMap.fourth,
+            entitiesMap.third,
+            entitiesMap.second,
+            entitiesMap.first,
+          ],
+          meta: {
+            current_page: 1,
+            last_page: 1,
+            per_page: 15,
+            total: 4,
+          },
+        },
+      },
+    ];
+
+    return { entitiesMap, arrange };
+  }
+
+  static arrangeUnsorted() {
+    const faker = CastMember.fake().anActor();
+
+    const entitiesMap = {
+      a: faker.withName('a').build(),
+      AAA: faker.withName('AAA').build(),
+      AaA: faker.withName('AaA').build(),
+      b: faker.withName('b').build(),
+      c: faker.withName('c').build(),
+    };
+
+    const arrange = [
+      {
+        send_data: {
+          page: 1,
+          per_page: 2,
+          sort: 'name',
+          filter: {
+            name: 'a',
+          },
+        },
+        expected: {
+          entities: [entitiesMap.AAA, entitiesMap.AaA],
+          meta: {
+            current_page: 1,
+            last_page: 2,
+            per_page: 2,
+            total: 3,
+          },
+        },
+      },
+      {
+        send_data: {
+          page: 2,
+          per_page: 2,
+          sort: 'name',
+          filter: {
+            name: 'a',
+          },
+        },
+        expected: {
+          entities: [entitiesMap.a],
+          meta: {
+            current_page: 2,
+            last_page: 2,
+            per_page: 2,
+            total: 3,
+          },
+        },
+      },
+      {
+        send_data: {
+          page: 1,
+          per_page: 2,
+          sort: 'name',
+          filter: {
+            name: 'a',
+            type: CastMemberTypes.DIRECTOR,
+          },
+        },
+        expected: {
+          entities: [],
+          meta: {
+            current_page: 1,
+            last_page: 0,
+            per_page: 2,
+            total: 0,
+          },
+        },
+      },
+    ];
+
+    return { entitiesMap, arrange };
+  }
+}
