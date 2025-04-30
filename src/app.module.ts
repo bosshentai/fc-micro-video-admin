3884b31a8b8e35712616e221cafcaa2adad64590
Bosshentai <baptistamhernani@gmail.com>
2025-04-30 17:18:08 +0000

    fix: docker-compose for dev feat: add dockerignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { GenresModule } from './nest-modules/genres-module/genres.module';
 import { VideosModuleModule } from './nest-modules/videos-module/videos.module';
 import { EventModule } from './nest-modules/event-module/event.module';
 import { UseCaseModule } from './nest-modules/use-case-module/use-case.module';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 
 @Module({
   imports: [
@@ -20,6 +21,10 @@ import { UseCaseModule } from './nest-modules/use-case-module/use-case.module';
     CastMembersModule,
     GenresModule,
     VideosModuleModule,
+    RabbitMQModule.forRoot({
+      uri: 'amqp://admin:admint@rabbitmq:5672',
+      // connectionInitOptions: { wait: false },
+    }),
   ],
 })
 export class AppModule {}
